6e04f4f4ddb95a128eae93e38c9289fa66c18cb9
Alessandro Tibaldi <alessandrotibaldi93@gmail.com>
2021-05-11 15:57:06 +0200

    Create project

---
+++ src/test/java/it/prisma/prismabooking/PrismaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prisma.prismabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
