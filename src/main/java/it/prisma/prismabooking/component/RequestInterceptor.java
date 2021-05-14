88acc211d07ee09f938c42e3099549646a35defb
Alessandro Tibaldi <alessandrotibaldi93@gmail.com>
2021-05-14 15:08:15 +0200

    Add request logging service

---
+++ src/main/java/it/prisma/prismabooking/component/RequestInterceptor.java
@@ -0,0 +1,32 @@
+package it.prisma.prismabooking.component;
+
+import it.prisma.prismabooking.service.RequestLoggingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    RequestLoggingService requestLoggingService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
+        requestLoggingService.logRequest(request, null);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception e) {
+        requestLoggingService.logResponse(request, response, null);
+    }
+}
---
+++ src/main/java/it/prisma/prismabooking/service/RequestLoggingService.java
@@ -0,0 +1,64 @@
+package it.prisma.prismabooking.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class RequestLoggingService {
+
+    public void logRequest(HttpServletRequest request, Object body) {
+        if (request.getRequestURI().equals("/error"))
+            return;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, String> params = buildParametersMap(request);
+
+        stringBuilder.append("Request ");
+        stringBuilder.append(request.getMethod()).append(" ");
+        stringBuilder.append(request.getRequestURI());
+
+        if (!params.isEmpty())
+            stringBuilder.append(" params=").append(params.toString().replace("=", ":")).append(" ");
+
+        if (body != null)
+            stringBuilder.append(" body=[").append(body).append("]");
+
+        log.info(stringBuilder.toString());
+    }
+
+    public void logResponse(HttpServletRequest request, HttpServletResponse response, Object body) {
+        if (request.getRequestURI().equals("/error"))
+            return;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, String> params = buildParametersMap(request);
+
+        stringBuilder.append("Response ");
+        String statusCode = String.valueOf(response.getStatus());
+        stringBuilder.append(statusCode).append(" ");
+        stringBuilder.append(request.getMethod()).append(" ");
+        stringBuilder.append(request.getRequestURI()).append(" ");
+
+        if (!params.isEmpty())
+            stringBuilder.append(" params=").append(params.toString().replace("=", ":")).append(" ");
+
+        if (statusCode.matches("^2.."))
+            log.info(stringBuilder.toString());
+        else
+            log.error(stringBuilder.toString());
+    }
+
+    private Map<String, String> buildParametersMap(HttpServletRequest request) {
+        Map<String, String> result = new HashMap<>();
+        Map<String, String[]> params = request.getParameterMap();
+
+        params.forEach((key, value) -> result.put(key, String.join(", ", value)));
+        return result;
+    }
+}
